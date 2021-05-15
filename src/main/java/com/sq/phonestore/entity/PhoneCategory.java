d5ec34da5f199520c31919863e52064085d24b86
sunqiao <760526001@qq.com>
2021-05-15 22:10:38 +0800

    初始化工程

---
+++ src/main/java/com/sq/phonestore/entity/PhoneCategory.java
@@ -0,0 +1,8 @@
+package com.sq.phonestore.entity;
+
+/**
+ * @author SunQiao
+ * @create 2021-05-15 22:09
+ */
+public class PhoneCategory {
+}
---
+++ src/test/java/com/sq/phonestore/PhonestoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sq.phonestore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhonestoreApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
